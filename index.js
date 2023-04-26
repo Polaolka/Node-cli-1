495ee0d4785bb61743dddd7f9976684d2b2f234d
Olga Polishchuk <olgapolichuk@ukr.net>
2023-04-26 20:28:19 +0300

    add HW

---
+++ index.js
@@ -0,0 +1,48 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allcontacts = await contacts.listContacts();
+      console.table(allcontacts);
+      break;
+
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      console.table(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+      console.table(newContact);
+      break;
+
+    case "updateById":
+      const updatedContact = await contacts.updatedContactById(id, { name, email, phone });
+      console.table(updatedContact);
+      break;
+
+    case "remove":
+        const deleteContact = await contacts.removeContact(id);
+        console.table(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
